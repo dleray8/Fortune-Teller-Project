b744fb9f608dcf9ee6e0abf25ffcbd3cbafa1aa2
dleray8 <61877738+dleray8@users.noreply.github.com>
2020-03-06 09:44:14 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,7 @@
+$("button").click(function(){
+let name=$(".class1").val();
+let nightmare=$(".class2").val();
+let number=$(".class3").val();
+$("p").text(' In the year 2050,${name} will ${nightmare} ${number} ' );
+
+});
